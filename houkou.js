b7db30a44a19998f72a352c3f6864c5c4489c2e6
defaude <git@defaude.info>
2012-02-04 10:41:04 +0100

    Make sure Houkou won't attempt to add "parameters", "pattern" and "build" as globals when the user called it without using "new"

--- houkou.js
+++ houkou.js
@@ -7,6 +7,9 @@ function merge(a, b) {
 }
 
 function Houkou(pattern) {
+  if (!(this instaceof Houkou) {
+    return new Houkou(pattern);
+  }
   this.parameters = (pattern.match(/:([a-z_]+)/gi) || []).map(function(p) { return p.substr(1); });
   this.pattern = new RegExp(("^" + pattern.replace(/:[a-z_]+?!\(|:[a-z_]+$/gi, "(.+?)").replace(/:[a-z_]+/gi, "") + "$").replace(/\//g, "\\/"));
   this.build = new Function("v", 'return "' + pattern.replace(/:([a-z_]+)(?:\(.*?\))?/gi, '" + v["$1"] + "') + '";');
